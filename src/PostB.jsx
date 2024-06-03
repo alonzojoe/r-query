69f11fa394d4314c2028d64d9eae77f99bd681f3
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-06-03 16:51:35 +0800

    use destructuring items from useQuery

--- src/PostB.jsx
+++ src/PostB.jsx
@@ -4,18 +4,18 @@ import { getPosts } from "./api/posts";
 const POST_LIMIT = 10;
 
 const PostB = () => {
-  const postQuery = useQuery({
+  const { isLoading, isError, error, data } = useQuery({
     queryKey: ["posts", "B", { limit: POST_LIMIT }],
     queryFn: () => getPosts(2000, POST_LIMIT),
   });
 
-  if (postQuery.isLoading) return <p>Loading....</p>;
-  if (postQuery.isError) return <p>{postQuery.error.message}</p>;
+  if (isLoading) return <p>Loading....</p>;
+  if (isError) return <p>{error.message}</p>;
 
   return (
     <>
-      {postQuery.data &&
-        postQuery.data.map((post) => (
+      {data &&
+        data.map((post) => (
           <div key={post.id}>
             <h1>
               {post.id} {post.title}

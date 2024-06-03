816262a71ae97252eb7893dea8b14a9d96b24893
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-06-03 10:29:04 +0800

    replacing useEffect with useQuery to fetch data from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,41 @@
-import { useEffect, useState } from "react";
+import { useQuery, useMutation } from "@tanstack/react-query";
 import "./App.css";
 import api from "./api";
-function App() {
-  const [data, setData] = useState([]);
-  const getData = async () => {
-    const response = await api.get("/posts");
-    const posts = await response.data.posts;
-    setData(posts);
-  };
 
-  useEffect(() => {
-    getData();
-  }, []);
+const fetchPosts = async (interval) => {
+  const response = await new Promise((resolve, reject) => {
+    setTimeout(async () => {
+      try {
+        const res = await api.get("/posts");
+        resolve(res);
+      } catch (error) {
+        console.log("error", error);
+        reject(error);
+      }
+    }, interval);
+  });
+
+  console.log("API Response:", response);
+  return response.data.posts;
+};
+
+function App() {
+  const postQuery = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => fetchPosts(2000),
+  });
+
+  if (postQuery.isLoading) return <p>Loading...</p>;
+  if (postQuery.isError)
+    return <pre>{JSON.stringify(postQuery.error.message)}</pre>;
 
   return (
     <>
       <h1>React Query</h1>
       <div>
-        <pre>{JSON.stringify(data)}</pre>
+        {postQuery.data.map((post) => (
+          <pre key={post.id}>{JSON.stringify(post)}</pre>
+        ))}
       </div>
     </>
   );
